cc495ee7e0cbefaf8e24fcde30b00c4df6146826
Gopher <gopher0428@gmail.com>
2019-03-05 12:55:28 +0800

    部署

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Gopher_blog',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
